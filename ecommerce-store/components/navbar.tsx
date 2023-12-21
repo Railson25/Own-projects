f1f71359ad730b88a956485e02921cd3dcc93d1e
Railson25 <railsonsantiago123@gmail.com>
2023-12-21 14:24:23 -0300

    <feat>: navbar component

---
+++ ecommerce-store/components/navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Container } from "./ui/container";
+import { MainNav } from "./main-nav";
+import getCategories from "@/actions/get-categories";
+
+export const Navbar = async () => {
+  const categories = await getCategories();
+
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">Store</p>
+          </Link>
+          <MainNav data={categories} />
+        </div>
+      </Container>
+    </div>
+  );
+};
