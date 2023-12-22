3ec7b4620b60a8ec5091985f678557ba4c808fbb
Railson25 <railsonsantiago123@gmail.com>
2023-12-22 13:07:36 -0300

    <feat>: icon button component

---
+++ ecommerce-store/components/ui/icon-button.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { cn } from "@/lib/util";
+import { MouseEventHandler } from "react";
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  icon: React.ReactElement;
+  className?: string;
+}
+
+const IconButton = ({ icon, className, onClick }: IconButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
+        className
+      )}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default IconButton;
