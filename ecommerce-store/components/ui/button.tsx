e72405c54d4886481edb5cc632a451a0848edb18
Railson25 <railsonsantiago123@gmail.com>
2023-12-26 14:31:35 -0300

    <fix>: add all props to our button

--- ecommerce-store/components/ui/button.tsx
+++ ecommerce-store/components/ui/button.tsx
@@ -8,11 +8,12 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
     return (
       <button
-        ref={ref}
         className={cn(
           `w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`,
           className
         )}
+        ref={ref}
+        {...props}
       >
         {children}
       </button>
