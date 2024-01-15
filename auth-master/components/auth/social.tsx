2812d698eb05bd29e650127e990b1437e6b923be
Railson25 <railsonsantiago123@gmail.com>
2024-01-15 13:56:58 -0300

    <feat>: adding onClick function to login with Oauth

--- auth-master/components/auth/social.tsx
+++ auth-master/components/auth/social.tsx
@@ -3,16 +3,34 @@
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "../ui/button";
 import { FaGithub } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 interface SocialProps {}
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-full"
+        onClick={() => onClick("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-full"
+        onClick={() => onClick("github")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
