88fe183096bf3af29fff87b9da8ad26dbacaae75
Railson25 <railsonsantiago123@gmail.com>
2024-01-16 11:54:02 -0300

    <feat>: send email in register

--- auth-master/actions/register.ts
+++ auth-master/actions/register.ts
@@ -2,6 +2,7 @@
 
 import { getUserByEmail } from "@/data/user";
 import { db } from "@/lib/db";
+import { sendVerificationEmail } from "@/lib/mail";
 import { generateVerificationToken } from "@/lib/tokens";
 import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcryptjs";
@@ -32,6 +33,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   });
 
   const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
   return { success: "Confirmation email sent!" };
 };
