6f5ad6cc04b39643ae4b66550c4d5c7c80d6d9fd
Railson25 <railsonsantiago123@gmail.com>
2024-01-18 12:45:16 -0300

    <feat>: forgot password button

--- auth-master/components/auth/login-form.tsx
+++ auth-master/components/auth/login-form.tsx
@@ -22,6 +22,7 @@ import { FormSuccess } from "../form.success";
 import { login } from "@/actions/login";
 import { useState, useTransition } from "react";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 export const LoginForm = () => {
   const searchParams = useSearchParams();
@@ -96,6 +97,14 @@ export const LoginForm = () => {
                       type="password"
                     />
                   </FormControl>
+                  <Button
+                    size="sm"
+                    variant="link"
+                    asChild
+                    className="px-0 font-normal"
+                  >
+                    <Link href="/auth/reset">Forgot password?</Link>
+                  </Button>
                   <FormMessage />
                 </FormItem>
               )}
