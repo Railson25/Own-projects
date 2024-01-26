152f0fb8c766b58ad2da08800a326f49bc8a409a
Railson25 <railsonsantiago123@gmail.com>
2024-01-26 11:38:26 -0300

    feat(Settings Page): Creating settings page
    This commit will allow user to change thei name in real time and updated
    their session imediatly, so that way they will also see the result of
    this change in a server/clinet page

---
+++ auth-master/actions/settings.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { getUserById } from "@/data/user";
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { SettingsSchema } from "@/schemas";
+import * as z from "zod";
+
+export const Settings = async (values: z.infer<typeof SettingsSchema>) => {
+  const user = await currentUser();
+
+  if (!user) {
+    return { error: "Unauthorized" };
+  }
+
+  const dbUser = await getUserById(user.id);
+
+  if (!dbUser) {
+    return { error: "Unauthorized" };
+  }
+
+  await db.user.update({
+    where: { id: dbUser.id },
+    data: { ...values },
+  });
+
+  return { success: "Settings Updated!" };
+};
