bf3fcc20472fc2f8355d50cfd29b83c38fa68f8b
Railson25 <railsonsantiago123@gmail.com>
2024-01-27 17:18:21 -0300

    feat(callbackUrl): Saving the last url
    This commit will allow user to go back at the last page that they were
    before logout

--- auth-master/components/auth/social.tsx
+++ auth-master/components/auth/social.tsx
@@ -5,13 +5,17 @@ import { Button } from "../ui/button";
 import { FaGithub } from "react-icons/fa";
 import { signIn } from "next-auth/react";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useSearchParams } from "next/navigation";
 
 interface SocialProps {}
 
 export const Social = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
   const onClick = (provider: "google" | "github") => {
     signIn(provider, {
-      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+      callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
     });
   };
 
