af6d8e426295ef0db7b6fb272b63c28298f93e53
Railson25 <railsonsantiago123@gmail.com>
2024-02-07 19:21:55 -0300

    feat(Navbar): Navbar styled
    This commiw will add a navbar to our page with some styles and with a
    button to login and logout our aplication

---
+++ stay-savvy/components/layout/navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { UserButton, useAuth } from "@clerk/nextjs";
+import { Container } from "../container";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import { SearchInput } from "../search-input";
+
+export const Navbar = () => {
+  const router = useRouter();
+
+  const { userId } = useAuth();
+
+  return (
+    <div className="sticky top-0 border border-b-primary/10 bg-secondary">
+      <Container>
+        <div className="flex justify-between">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <Image src="/logo.svg" alt="Logo image" width="30" height="30" />
+            <div className="font-bold text-xl">StaySavvy</div>
+          </div>
+          <SearchInput />
+          <div className="flex gap-3 items-center">
+            <div>Theme</div>
+            <UserButton afterSignOutUrl="/" />
+            {!userId && (
+              <>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={() => router.push("/sign-in")}
+                >
+                  Sign in
+                </Button>
+                <Button size="sm" onClick={() => router.push("/sign-up")}>
+                  Sign up
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
