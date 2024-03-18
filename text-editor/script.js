5de77c556244b61b3a5d9ceed1a6ec3b3086d732
Railson25 <railsonsantiago123@gmail.com>
2024-03-18 19:46:19 -0300

    <fix>: att branch main

---
+++ text-editor/script.js
@@ -0,0 +1,13 @@
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+let fontList;
