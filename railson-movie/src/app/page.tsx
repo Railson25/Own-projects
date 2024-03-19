2a5bd43b8fb9774743cc545aed0ec2bcc77abe85
Railson25 <railsonsantiago123@gmail.com>
2024-03-19 20:06:11 -0300

    feat(fetch): Data
    This commit will fetch data from api TMDB

--- railson-movie/src/app/page.tsx
+++ railson-movie/src/app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div>24rgeretahwgrt</div>;
+import { Result } from "@/components/results";
+import type { Movie } from "@/type/api-type";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }: any) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const movies: Movie[] = data.results;
+
+  console.log("nsdgvijuoaerngijakoSWEEGN", movies);
+
+  return (
+    <div>
+      <Result movies={movies} />
+    </div>
+  );
 }
