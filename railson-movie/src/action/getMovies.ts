3db278ee9964b6ff788c984aa7fecb3f3fcc2d8f
Railson25 <railsonsantiago123@gmail.com>
2024-03-20 13:49:49 -0300

    feat(loading): Handle loading
    This commit will add a nice loading image to our users

--- railson-movie/src/action/getMovies.ts
+++ railson-movie/src/action/getMovies.ts
@@ -6,7 +6,8 @@ export const getMovies = async (genre: string) => {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 600 } }
   );
   const data = await res.json();
   if (!res.ok) {
