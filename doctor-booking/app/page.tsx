888db47a34d684060483b366abc808e4f2c66382
Railson25 <railsonsantiago123@gmail.com>
2024-04-02 18:54:48 -0300

    feat(shadcn): Configuration
    This commit will add shadcn to our project

--- doctor-booking/app/page.tsx
+++ doctor-booking/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>wesrgr</h1>
+      <Button>Login</Button>
     </div>
   );
 }
