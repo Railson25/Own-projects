2084ca1da792fc7ac7b037844dc1edbd2525519b
Railson25 <railsonsantiago123@gmail.com>
2024-04-04 11:38:27 -0300

    feat(strapi): Config
    This commit will create a strapi Api configuration following the oficial
    documentation! also you have to set up coudinary plugin in the strapi
    dashboard, allow public users to list our slider collection, and more to
    work propertly and much more

---
+++ doctor-appointments/config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = () => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
