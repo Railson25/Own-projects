edc5d2dddf4b719b5571cd568aab9fd8beaee73f
Railson25 <railsonsantiago123@gmail.com>
2024-04-15 14:50:24 -0300

    feat(right): Right bar
    This commit will create a right side bar style

--- vibe-zone/components/layout/right-side-bar.tsx
+++ vibe-zone/components/layout/right-side-bar.tsx
@@ -1,7 +1,14 @@
 export const RightSideBar = () => {
   return (
-    <div>
-      <h1>right</h1>
+    <div className="sticky right-0 top-0 z-20 h-screen w-[300px] xl:w-[350px] flex flex-col gap-12 overflow-auto pl-6 pr-10 py-6 max-lg:hidden">
+      <div className="flex flex-col gap-4">
+        <h4 className="text-heading4-bold text-light-1">Following</h4>
+        <div className="flex flex-col gap-4">mapping users</div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-heading4-bold text-light-1"> Suggested People</h4>
+        <div className="flex flex-col gap-4">mapping users</div>
+      </div>
     </div>
   );
 };
