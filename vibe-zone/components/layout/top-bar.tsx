a0a210520fc0be9a624872ba0c5e12f2a85bab74
Railson25 <railsonsantiago123@gmail.com>
2024-04-15 14:30:13 -0300

    feat(top): Top bar
    This commit will create a topBar component

--- vibe-zone/components/layout/top-bar.tsx
+++ vibe-zone/components/layout/top-bar.tsx
@@ -1,7 +1,53 @@
+"use client";
+
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { Add, Logout, Search } from "@mui/icons-material";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export const TopBar = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
   return (
-    <div>
-      <h1>sqef</h1>
+    <div className="flex justify-between items-center mt-6">
+      <div className="relative">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search post, people, ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Search className="search-icon" onClick={() => {}} />
+      </div>
+      <button
+        className="create-post-btn"
+        onClick={() => router.push("/create-post")}
+      >
+        <Add />
+        <p>Create a post</p>
+      </button>
+      <div className="flex gap-3">
+        <SignedIn>
+          <SignOutButton>
+            <div className="flex cursor-pointer items-center md:hidden">
+              <Logout sx={{ color: "white", fontSize: "32px" }} />
+            </div>
+          </SignOutButton>
+        </SignedIn>
+        <Link href="/">
+          <Image
+            alt="profile photo"
+            src="/images/Railson.jpg"
+            width={50}
+            height={50}
+            className="rounded-full md:hidden"
+          />
+        </Link>
+      </div>
     </div>
   );
 };
