a29cedb2630ea3fd48438f7d4fb3002ec52673fe
Railson25 <railsonsantiago123@gmail.com>
2024-04-18 14:30:56 -0300

    feat(functions): Amazon url
    This commit will add a function to check if is a valid url link, also
    will create a dinamic toast and loading button check

--- pricewise/app/layout.tsx
+++ pricewise/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import { Toaster } from "react-hot-toast";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={spaceGrotesk.className}>
         <main className="max-w-[1440px] mx-auto">
+          <Toaster />
           <Navbar />
           {children}
         </main>
