0ed4df678a228372be383ecb608b72caf541fea7
Railson25 <railsonsantiago123@gmail.com>
2024-04-22 10:13:39 -0300

    feat(products): Product card
    This commit will create a product card and will fetch all products for
    our database

---
+++ pricewise/app/products/[id]/page.tsx
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>WEGwer</h1>
+    </div>
+  );
+};
+
+export default ProductDetails;
---
+++ pricewise/components/product-card.tsx
@@ -0,0 +1,40 @@
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <Link
+      href={`/products/${product._id}`}
+      className="sm:w-[292px] sm:max-w-[292px] w-full flex-1 flex flex-col gap-4 rounded-md shadow-md p-5 "
+    >
+      <div className="flex-1 relative flex flex-col gap-5 p-4 rounded-md">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={200}
+          height={200}
+          className="max-h-[250px] object-contain w-full h-full bg-transparent"
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <h3 className="text-secondary text-xl leading-6 font-semibold truncate">
+          {product.title}
+        </h3>
+        <div className="flex justify-between">
+          <p className="text-black opacity-50 text-lg capitalize">
+            {product.category}
+          </p>
+          <p className="text-black text-lg font-semibold">
+            <span>{product.currency}</span>
+            <span>{product.currentPrice}</span>
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
