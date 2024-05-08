5cd1b691eb4e77590c2f4f2e5d997302b7d4af29
Railson25 <railsonsantiago123@gmail.com>
2024-05-08 15:48:54 -0300

    feat(liked-post): Page
    This commit will create a likes post page

--- railsongram/src/_root/pages/liked-post.tsx
+++ railsongram/src/_root/pages/liked-post.tsx
@@ -1,5 +1,26 @@
+import { GridPostList } from "@/components/shared/grid-post-list";
+import { Loader } from "@/components/shared/loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+
 const LikedPosts = () => {
-  return <h1 className="text-black">djiughi</h1>;
+  const { data: currentUser } = useGetCurrentUser();
+
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-4">No liked posts</p>
+      )}
+
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
 };
 
 export default LikedPosts;
